c2b0a1353c9911d46da3d4828d5413e4d98ce950
Masaki Hosobuchi <masah0102@gmail.com>
2021-09-09 21:15:30 +0900

    Initial Commit

---
+++ everyoneMeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  everyoneMeal
+//
+//  Created by 細渕雅貴 on 2021/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
